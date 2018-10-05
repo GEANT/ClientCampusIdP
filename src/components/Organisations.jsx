b2080b85335651639ce5177722450fd358a8c7fe
Michael Schmidt <Michael.Schmidt@lrz.de>
2018-10-05 12:18:37 +0200

    New descriptive text

--- src/components/Organisations.jsx
+++ src/components/Organisations.jsx
@@ -1,28 +1,34 @@
 import React from "react";
-import { PageHeader } from "react-bootstrap";
+import { PageHeader, Grid, Row } from "react-bootstrap";
+import TextBlock from "./TextBlock";
 
 const Organisation = () => {
   return (
     <React.Fragment>
       <PageHeader>Campus IdP for R&E organisations</PageHeader>
-      <p>
-        The Campus IdP Platform aims to providing Federation Operators (FedOps)
-        a way to easily deploy and manage cloud-based Campus IdP for their Home
-        Organizations: It integrates tools currently related to the operational
-        workflow of federation operators, but belonging however to different
-        functional domains (Metadata Management and Archiving, Cloud
-        infrastructure, eased IdP configuration, Host Certificates, Software
-        Repositories). The overall goal of GEANT providing a Campus IdP platform
-        is to ease the daily life of FedOps by integrating in a unique system
-        the Management and Archiving of Metadata, installation and configuration
-        tools to spawn IdP instances on the Cloud, Monitoring probes, in a
-        scalable and convenient fashion. We deal here with the listing of the
-        functionality which will be provided by the system to NREN operators to
-        support their role of Campus IdP Cloud providers to Home Organizations,
-        splitting it into 5 main categories: IdP Metadata Management, IdP
-        Configuration, Spawning of new Campus IdP instances, Monitoring,
-        Statistics (Campus IdP platform + Jagger).
-      </p>
+      <Grid>
+        <Row>
+          <TextBlock
+            title="Create an IdP"
+            text="The Campus IdP Platform aims to providing Operators
+            a way to easily deploy and manage cloud-based Campus IdP
+            for their Home Organizations: It integrates tools currently related
+            to the operational workflow of federation operators, but belonging
+            however to different functional domains (Metadata Management and
+            Archiving, Cloud infrastructure, eased IdP configuration, Host
+            Certificates, Software Repositories)."
+          />
+          <TextBlock
+            title="Manage your IdP"
+            text="The overall goal of GEANT
+          providing a Campus IdP platform is to ease the daily life of FedOps
+          by integrating in a unique system the Management and Archiving of
+          Metadata, installation and configuration tools to spawn IdP
+          instances on the Cloud, Monitoring probes, in a scalable and
+          convenient fashion."
+          />
+        </Row>
+      </Grid>
     </React.Fragment>
   );
 };
